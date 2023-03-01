0106b44533a0d1eac1f61507ec6dedd2ea17299e
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-03-01 20:15:30 +0400

    Add Dictionaries

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -1,14 +1,38 @@
-﻿using System.Windows;
+﻿using Source.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Windows;
 
 namespace Source;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
+    ObservableCollection<Car>Cars { get; set; }
+    CancellationTokenSource token;
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = this;
+
+    }
+    private void StartBtn_Click(object sender, RoutedEventArgs e)
+    {
+        token = new();
+        if(TgButton.IsChecked == true) 
+            MultiCars(token);
+        else
+            SingleCars(token);
+    }
+
+    private void SingleCars(CancellationTokenSource token)
+    {
+        CarsList.Items.Clear();
+    }
+
+    private void MultiCars(CancellationTokenSource token)
+    {
+        CarsList.Items.Clear();
+
     }
 }
