2136301b1fba23e0ac4df60033192c258ff43c1a
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-03-03 10:34:42 +0400

    Timer Repaired

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Text.Json;
 using System.Threading;
@@ -14,17 +15,19 @@ public partial class MainWindow : Window
 {
     ObservableCollection<Car> Cars { get; set; }
     CancellationTokenSource token;
-    DispatcherTimer timer = new DispatcherTimer();
+    Stopwatch stopwatch;
+
     public MainWindow()
     {
         InitializeComponent();
         DataContext = this;
         Cars = new();
         CarsList.ItemsSource = Cars;
+        stopwatch = new();
     }
+
     private void StartBtn_Click(object sender, RoutedEventArgs e)
     {
-        timer.Start();
         token = new();
         if (TgButton.IsChecked == true)
             MultiCars(token.Token);
@@ -41,6 +44,7 @@ public partial class MainWindow : Window
         Cars.Clear();
         new Thread(() =>
         {
+            stopwatch = Stopwatch.StartNew();
             var directory = new DirectoryInfo(@"..\..\..\FakeDatas");
             foreach (var file in directory.GetFiles())
             {
@@ -54,20 +58,16 @@ public partial class MainWindow : Window
                     if (carlist is not null)
                         foreach (var car in carlist)
                         {
-                            if (token.IsCancellationRequested)
-                            {
-                                Dispatcher.Invoke(() => { Timertxtbox.Text = timer.Interval.ToString(); });
-                                break;
-                            }
+                            if (token.IsCancellationRequested) break;
 
                             Dispatcher.Invoke(() => Cars?.Add(car));
-                            Dispatcher.Invoke(() => { Timertxtbox.Text = timer.Interval.ToString(); });
                             Dispatcher.Invoke(() => { StartBtn.IsEnabled = false; });
+                            Dispatcher.Invoke(() => { Timertxtbox.Text = stopwatch.Elapsed.ToString("mm\\:ss\\.ff"); });
                             Thread.Sleep(200);
                         }
                 }
             }
-            Dispatcher.Invoke(() => { Timertxtbox.Text = timer.Interval.ToString(); });
+            Dispatcher.Invoke(() => { Timertxtbox.Text = stopwatch.Elapsed.ToString("mm\\:ss\\.ff"); });
         }).Start();
     }
 
@@ -76,6 +76,7 @@ public partial class MainWindow : Window
         Cars.Clear();
         var sync = new object();
         var dirInfo = new DirectoryInfo(@"..\..\..\FakeDatas");
+        stopwatch = Stopwatch.StartNew();
         foreach (var file in dirInfo.GetFiles())
         {
             if (file.Extension == ".json")
@@ -90,13 +91,14 @@ public partial class MainWindow : Window
                         {
                             if (token.IsCancellationRequested)
                             {
-                                Dispatcher.Invoke(() => { Timertxtbox.Text = timer.Interval.ToString(); });
+                                Dispatcher.Invoke(() => { Timertxtbox.Text = stopwatch.Elapsed.ToString("mm\\:ss\\.ff"); });
                                 break;
                             }
                             lock (sync)
                                 Dispatcher.Invoke(() => Cars?.Add(car));
 
-                            Dispatcher.Invoke(() => { Timertxtbox.Text = timer.Interval.ToString(); });
+
+                            Dispatcher.Invoke(() => { Timertxtbox.Text = stopwatch.Elapsed.ToString("mm\\:ss\\.ff"); });
                             Dispatcher.Invoke(() => { StartBtn.IsEnabled = false; });
                             Thread.Sleep(100);
                         }
